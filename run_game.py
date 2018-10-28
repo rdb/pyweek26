adfc8f3cbfab8ccbde3464881b7ec8ffd0de957e
rdb <git@rdb.name>
2018-10-28 21:08:57 +0100

    Display loud error message if user fails to pip install requirements

--- run_game.py
+++ run_game.py
@@ -1,4 +1,24 @@
 #! /usr/bin/env python
 
+import sys
+
+try:
+    import panda3d
+    import numpy
+except ImportError as ex:
+    print("""
+===================================================
+
+This game requires Panda3D 1.10 and numpy >1.8.0
+
+Please run the following command to install them:
+
+    pip install -r requirements.txt
+
+===================================================
+""")
+    print(repr(ex))
+    sys.exit(1)
+
 from gamelib import main
 main()
